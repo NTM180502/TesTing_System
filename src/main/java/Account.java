15f79ae043e71d736219ca06b42d4d5b3c5da1d0
Mạnh <nguyentangmanh180502@gmail.com>
2024-05-13 21:37:45 +0700

    Create lesson-02

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -8,4 +8,24 @@ public class Account {
      Department department;
      Position position;
      LocalDate createdDate;
+
+
+     Account () {
+     }
+     Account (int id, String email, String username, String fullName, String firstname, String lastname) {
+          this.id = id;
+          this.email = email;
+          this.username = username;
+          this.fullName = firstname + lastname;
+
+     }
+
+     Account (int id, String email, String username, String fullName, String firstname, String lastname, Position position ) {
+          this.id = id;
+          this.email = email;
+          this.username = username;
+          this.fullName = firstname + lastname;
+          this.position = position;
+          this.createdDate = LocalDate.now();
+     }
 }
